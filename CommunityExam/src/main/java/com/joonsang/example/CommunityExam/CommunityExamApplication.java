4f3ecdfd4231d026ff025664cab56c77b41ff06c
joonsang <gufrus@naver.com>
2021-02-09 10:23:51 +0900

    [2021.02.09] init

---
+++ CommunityExam/src/main/java/com/joonsang/example/CommunityExam/CommunityExamApplication.java
@@ -0,0 +1,13 @@
+package com.joonsang.example.CommunityExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityExamApplication.class, args);
+	}
+
+}
---
+++ CommunityExam/src/test/java/com/joonsang/example/CommunityExam/CommunityExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joonsang.example.CommunityExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
