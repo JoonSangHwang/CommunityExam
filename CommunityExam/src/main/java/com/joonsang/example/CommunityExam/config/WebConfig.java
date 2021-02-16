1b28842a2bb1344ca36142ce8022423f7cd48a14
joonsang <gufrus@naver.com>
2021-02-16 16:46:30 +0900

    [2021.02.16] Session Timeout Configure

--- CommunityExam/src/main/java/com/joonsang/example/CommunityExam/config/WebConfig.java
+++ CommunityExam/src/main/java/com/joonsang/example/CommunityExam/config/WebConfig.java
@@ -1,11 +1,14 @@
 package com.joonsang.example.CommunityExam.config;
 
+import com.joonsang.example.CommunityExam.listener.SessionListener;
 import com.joonsang.example.CommunityExam.resolver.UserArgumentResolver;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.servlet.http.HttpSessionListener;
 import java.util.List;
 
 @Configuration
@@ -18,5 +21,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         // HandlerMethodArgumentResolver 추가
         argumentResolvers.add(userArgumentResolver);
+    }
+
+    @Bean
+    public HttpSessionListener httpSessionListener(){
+        return new SessionListener();
+
     }
 }
